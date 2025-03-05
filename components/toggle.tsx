d92df7e22be505b02b2219143425a7150cf5ab83
Abhish7k <abhishekbhosale.dev@gmail.com>
2025-03-05 14:12:23 +0530

    feat: theme switch
    added dark and light mode theme switcher

---
+++ components/toggle.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import * as React from "react";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "./ui/button";
+
+export function ModeSwitcher() {
+  const { setTheme, resolvedTheme } = useTheme();
+  const { setMetaColor } = useMetaColor();
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+    setMetaColor(
+      resolvedTheme === "dark"
+        ? META_THEME_COLORS.light
+        : META_THEME_COLORS.dark
+    );
+  }, [resolvedTheme, setTheme, setMetaColor]);
+
+  return (
+    <Button
+      variant="outline"
+      className="group/toggle h-8 w-8 px-0 hover:cursor-pointer"
+      onClick={toggleTheme}
+    >
+      <SunIcon className="hidden [html.dark_&]:block" />
+      <MoonIcon className="hidden [html.light_&]:block" />
+
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
+
+const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+};
+
+export function useMetaColor() {
+  const { resolvedTheme } = useTheme();
+
+  const metaColor = React.useMemo(() => {
+    return resolvedTheme !== "dark"
+      ? META_THEME_COLORS.light
+      : META_THEME_COLORS.dark;
+  }, [resolvedTheme]);
+
+  const setMetaColor = React.useCallback((color: string) => {
+    document
+      .querySelector('meta[name="theme-color"]')
+      ?.setAttribute("content", color);
+  }, []);
+
+  return {
+    metaColor,
+    setMetaColor,
+  };
+}
