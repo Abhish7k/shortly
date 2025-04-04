9fac50c967f764efd551a30cdc1f044d5d95ae5f
Abhish7k <abhishekbhosale.dev@gmail.com>
2025-04-04 16:10:01 +0530

    feat: shorten url action
    created server action for shorten url

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,29 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function isValidUrl(url: string): boolean {
+  try {
+    const urlObj = new URL(url);
+
+    return urlObj.protocol === "http:" || urlObj.protocol === "https:";
+  } catch (error) {
+    console.log("error while validating url", error);
+    return false;
+  }
+}
+
+export function ensureHttps(url: string): string {
+  if (!url.startsWith("https://")) {
+    return `https://${url}`;
+  }
+
+  if (url.startsWith("http://")) {
+    return url.replace("http://", "https://");
+  }
+
+  return url;
 }
---
+++ server/db/actions/urls/shorten-url.ts
@@ -0,0 +1,80 @@
+import { ApiResponse } from "@/lib/types";
+import { ensureHttps } from "@/lib/utils";
+import { z } from "zod";
+import { nanoid } from "nanoid";
+import { db } from "../..";
+import { urls } from "../../schema";
+import { revalidatePath } from "next/cache";
+
+type ShortenUrlResponse =
+  | {
+      success: true;
+      data: {
+        shortUrl: string;
+      };
+    }
+  | {
+      success: false;
+      error: string;
+    };
+
+const shortenUrlSchema = z.object({
+  url: z.string().url(),
+});
+
+export const shortenUrl = async (
+  formData: FormData
+): Promise<ShortenUrlResponse> => {
+  try {
+    const url = formData.get("url") as string;
+
+    const validatedFields = shortenUrlSchema.safeParse({ url });
+
+    if (!validatedFields.success) {
+      return {
+        success: false,
+        error:
+          validatedFields.error.flatten().fieldErrors.url?.[0] || "Invalid url",
+      };
+    }
+
+    const originalUrl = ensureHttps(validatedFields.data.url);
+
+    const shortCode = nanoid(6);
+
+    const exisitingUrl = await db.query.urls.findFirst({
+      where: (urls, { eq }) => eq(urls.shortCode, shortCode),
+    });
+
+    if (exisitingUrl) {
+      return shortenUrl(formData);
+    }
+
+    await db.insert(urls).values({
+      originalUrl,
+      shortCode,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+
+    const shortUrl = `${baseUrl}/r/${shortCode}`;
+
+    revalidatePath("/");
+
+    return {
+      success: true,
+      data: {
+        shortUrl,
+      },
+    };
+  } catch (error) {
+    console.log("failed to shorten url", error);
+
+    return {
+      success: false,
+      error: "Failed to shorten URL",
+    };
+  }
+};
