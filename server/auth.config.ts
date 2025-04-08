9bf3236d6dec3d5cb02cbb36bfd40a9e529b096a
Abhish7k <abhishekbhosale.dev@gmail.com>
2025-04-08 16:05:27 +0530

    feat: auth js
    done with auth js, added config for the google, github and email pass login

---
+++ server/auth.config.ts
@@ -0,0 +1,147 @@
+import { DefaultSession, NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
+import { users } from "./db/schema";
+import bcrypt from "bcryptjs";
+import { JWT } from "next-auth/jwt";
+import { db } from "./db";
+
+// Extend NextAuth types to include 'role' and 'id' in session, user, and JWT
+declare module "next-auth" {
+  interface User {
+    role?: "user" | "admin";
+  }
+
+  interface Session {
+    user: {
+      id: string;
+      role?: "user" | "admin";
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: "user" | "admin";
+  }
+}
+
+// next auth configuration
+export const authConfig: NextAuthConfig = {
+  secret: process.env.AUTH_SECRET,
+
+  // custom auth pages
+  pages: {
+    signIn: "/login",
+    signOut: "/logout",
+    error: "/login",
+    verifyRequest: "/verify-request",
+    newUser: "/register",
+  },
+
+  // to manage jwt
+  callbacks: {
+    // route access checks
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      const isOnAdmin = nextUrl.pathname.startsWith("/admin");
+
+      if (isOnAdmin) {
+        return isLoggedIn && auth?.user?.role === "admin";
+      } else if (isOnDashboard) {
+        if (isLoggedIn) return true;
+
+        return false;
+      } else if (isLoggedIn) {
+        return true;
+      }
+
+      return true;
+    },
+
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+        token.picture = user.image;
+        token.role = user.role;
+      }
+      return token;
+    },
+
+    session: async ({ session, token }) => {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+
+    // custom cred login: email + pass
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@exmple.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6),
+          })
+          .safeParse(credentials);
+
+        if (!parsedCredentials.success) return null;
+
+        const { email, password } = parsedCredentials.data;
+
+        // check if user exists in db
+        const [user] = await db
+          .select()
+          .from(users)
+          .where(eq(users.email, email.toLowerCase()));
+
+        if (!user) return null;
+
+        //  check if password matches
+        const passwordsMatch = await bcrypt.compare(
+          password,
+          user.password || ""
+        );
+
+        if (!passwordsMatch) return null;
+
+        // return user info for session jwt
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          image: user.image,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+};
