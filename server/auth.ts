7b03983fe591d022bad7881713fdf26affa7f1b3
Abhish7k <abhishekbhosale.dev@gmail.com>
2025-04-08 16:12:17 +0530

    feat: auth js
    added the route for next auth, do done with next auth/authjs

--- server/auth.ts
+++ server/auth.ts
@@ -1,11 +1,17 @@
 import NextAuth from "next-auth";
 import { db } from "./db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { authConfig } from "./auth.config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
   adapter: DrizzleAdapter(db),
 
   session: { strategy: "jwt" },
+
+  ...authConfig,
 });
