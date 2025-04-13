ca0b997bed2c124c789c4a025a977ca2f41c7cf2
Abhish7k <abhishekbhosale.dev@gmail.com>
2025-04-13 12:38:05 +0530

    feat: auth
    made a usernav comp to include all routes and logout btn after user logs in

---
+++ components/UserNav.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import {
+  DropdownMenuGroup,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
+import { LogOutIcon } from "lucide-react";
+import { signOut } from "next-auth/react";
+
+const UserNav = ({
+  name,
+  email,
+  image,
+}: {
+  name: string;
+  email: string;
+  image: string;
+}) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+          <Avatar className="h-10 w-10 rounded-full">
+            <AvatarImage src={image} alt="profile-image" />
+
+            <AvatarFallback>{name[0]}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel>
+          <div className="flex flex-col space-y-1">
+            <p className="text-lg font-medium leading-none">{name}</p>
+            <p className="leading-none text-muted-foreground">{email}</p>
+          </div>
+        </DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link
+              href="/dashboard"
+              className="w-full flex justify-start items-center gap-2 cursor-pointer font-medium"
+            >
+              Dashboard
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem className="w-full flex justify-start items-center gap-2 font-medium cursor-pointer">
+          <div
+            onClick={() => signOut()}
+            className="flex items-center gap-2 w-full"
+          >
+            <LogOutIcon className="text-xl" />
+            Log out
+          </div>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserNav;
